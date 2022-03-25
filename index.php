281743dfbdd20e554f1458bc0307504ea1ab35ea
Rishabh <rishaabh.kant@gmail.com>
2022-03-25 13:04:24 +0530

    Training project

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body>
+    <header>
+        <div class="head">
+
+            <div class="logo1">
+                <img src="logo.svg" alt="">
+            </div>
+            <h2 class="headt">
+                Oil Palm Information & Monitoring System: Niti Initiative
+
+            </h2>
+            <div class="logo2">
+                <img src="logo.svg" alt="">
+            </div>
+        </div>
+        <nav class="navb">
+            <ul>
+                <li><a href="home.html"> <b> Home </b></a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Government Initiative</a></li>
+                <li><a href="contact.html">Dashboard</a></li>
+                <li><a href="contact.html">Know Your State</a></li>
+                <li><a href="contact.html">Product Technologies</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="slider">
+                <img src="https://source.unsplash.com/1300x500/?farming" alt="">
+
+            </div>
+            <p class="content">
+                <b> <i> 
+
+                    Among all perennial crops, Oil palm crop is one of the highest oil yielding crops. The edible palm oil
+                    and palm kernel oil are both produced by the oil palm tree. Because of its high yielding capacity, this
+                    oil palm is known as the golden palm. Palm oil can be harvested throughout the year. After reaching the
+                    age of 5 years, oil palm can produce 20-25 MT fresh fruit bunches (FFB) per hectare with good planting
+                    material, irrigation, and correct management. In comparative terms, yield of palm oil is 5 times the
+                    yield of edible oil obtainable from traditional oilseeds. Millions of people and their families rely on
+                    oil palm agriculture and processing for survival. India currently produces palm oil on more than
+                    3,00,000 hectares of land and plans to cover an additional area of 6,50,000 hectares by 2025-26. India
+                    is the world's largest palm oil importer heavily dependent on Malaysia and Indonesia to meet about 60
+                    percent of its annual needs.
+                </i></b>
+                </p>
+                <div class="map">
+                <img src="images\MapChart_Map.png" alt="">
+                </div>
+    </main>
+    <footer>
+        <div class="footer_out">
+
+            <div class="add">
+                <h2><u>
+                        NITI Aayog
+                    </u>
+                </h2>
+                <p>Sansad Marg, Sansad Marg Area, New Delhi,
+                    Delhi 110001
+                </p>
+                <div class="oil">
+                    <h2>OIL Palm </h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis, aperiam sequi. Dignissimos
+                        debitis non fuga, quibusdam ea eaque voluptas ad repellendus nesciunt eligendi, labore maxime
+                        sapiente nobis reprehenderit cupiditate quo!</p>
+                </div>
+            </div>
+        </div>
+        <div class="footer_in">
+            <ul class="navb">
+                <li><a href="home.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Dashboard</a></li>
+                <li><a href="contact.html">Nodal Office</a></li>
+                <li><a href="contact.html">Refrences</a></li>
+
+            </ul>
+            <div class="copy">
+
+                <p>Copyright &copy; 2022 Niti Aayog agricultural Devision <b>Niti Aayog </p></b> </p>
+            </div>
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
